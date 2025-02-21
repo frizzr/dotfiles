620d8d5b9c4aa0fcc616e8a0f59e8019dc25b4d4
Frizzell-Carlton, Russ <russ.frizzell-carlton@ge.com>
2025-02-21 08:34:36 -0500

    Added copilot vim plugin.

--- dot_config/private_rnvim/private_init.lua
+++ dot_config/private_rnvim/private_init.lua
@@ -102,6 +102,7 @@ require("lazy").setup({
 'will133/vim-dirdiff',
 'williamboman/mason.nvim',
 'williamboman/mason-lspconfig.nvim',
+'github/copilot.vim',
 'mfussenegger/nvim-dap',
 {
   'neovim/nvim-lspconfig',
